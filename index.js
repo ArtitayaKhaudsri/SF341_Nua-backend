849272290050809640bfbda62aa84f2edc6f27af
ArtitayaKhaudsri <artitaya.kha@dome.tu.ac.th>
2022-11-12 17:16:47 +0700

    login and resigter by MongoDB

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const recipeRouter = require('./addRecipe/recipes/recipeRouter');
 const loginRouter = require('./user/router/loginRouter');
 const registerRouter = require('./user/router/registerRouter');
 
+const ip = 'current_local_ip'
+
+console.log(ip)
+
 app.use(cors());
 
 // Commands to convert JSON values to be able to retrieve and send values to MongoDB Atlas.
@@ -23,6 +27,6 @@ app.post("/register",registerRouter)
 
 const port = process.env.PORT || 3410;
 
-app.listen(port, function (req, res) {
+app.listen(port, '0.0.0.0',function (req, res) {
     console.log('Port is listing');
 });
